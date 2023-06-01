b179774b62ed3930c9efb58b3983bf7d1bf276d0
Mark Pallone <mpallone@riotgames.com>
2023-05-31 17:28:34 -0700

    Add JWT aliases

--- my-env.sh
+++ my-env.sh
@@ -110,3 +110,28 @@ githistory() {
     git lg -p -- $1
 }
 alias githist="githistory"
+
+decode_base64_url() {
+  local len=$((${#1} % 4))
+  local result="$1"
+  if [ $len -eq 2 ]; then result="$1"'=='
+  elif [ $len -eq 3 ]; then result="$1"'=' 
+  fi
+  echo "$result" | tr '_-' '/+' | openssl enc -d -base64
+}
+
+# Example usage: 
+# 
+#    decode_jwt 2 $pbToken
+# 
+# if pbToken holds a base-64 encoded JWT, this will pretty print it.
+# 
+decode_jwt(){
+   decode_base64_url $(echo -n $2 | cut -d "." -f $1) | jq .
+}
+
+# Decode JWT header
+alias jwth="decode_jwt 1"
+
+# Decode JWT Payload
+alias jwtp="decode_jwt 2"
