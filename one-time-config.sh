18444d6b4dd3211e3183c608d0dbb8dd3e03a144
Mark Pallone <mpallone@riotgames.com>
2026-07-06 15:21:21 -0700

    Add cloc install to one-time-config.sh

--- one-time-config.sh
+++ one-time-config.sh
@@ -6,3 +6,10 @@ git config --global user.name "Mark Pallone"
 git config --global user.email "mark.c.pallone@gmail.com"
 #                               ^ FILL THIS IN
 
+# CLI tools
+if command -v brew &> /dev/null; then
+  brew install cloc
+else
+  echo "Error: Homebrew not found; cannot install cloc. Install brew first: https://brew.sh" >&2
+fi
+
